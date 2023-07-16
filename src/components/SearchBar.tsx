2255aa44e73d7e1b66d43d77939b84f944f92bde
codingjwp <codingjwp@gmail.com>
2023-07-16 18:45:20 +0900

    Feat: 검색바 및 추천 검색어 목록 컴포넌트 생성:

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+import {ReactComponent as SearchSvg} from '../assets/search.svg'
+
+const SearchBar = () => {
+
+  return (
+    <SearchInputGroup>
+      <SearchInput type="search" id="searchbar" placeholder=' ' />
+      <SearchLabel htmlFor="searchbar">
+        <SearchSvg stroke='#ACB4BB' width={32} height={32}/> 질환명을 입력해주세요
+      </SearchLabel>
+      <SearchBtn type='button' aria-label='Search'>
+        <SearchSvg stroke='#FFFFFF' width={32} height={32}/>
+      </SearchBtn>
+    </SearchInputGroup>
+  );
+}
+
+const SearchInputGroup = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  background-color: #ffffff;
+  border: 0;
+  border-radius: 2rem;
+`
+
+const SearchInput = styled.input`
+  padding: 1rem 1.5rem 1rem .4rem;
+  border: 0;
+  outline: none;
+  font-size: 1.2rem;
+  width: calc(100% - 4rem);
+  &:not(:focus) + label, &:placeholder-shown + label {
+    opacity: 1;
+  };
+  &:focus + label, &:not(:placeholder-shown) + label {
+    opacity: 0;
+  };
+`
+
+const SearchLabel = styled.label`
+  position: absolute;
+  top: .6rem;
+  left: 1.9rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: #ACB4BB;
+  -webkit-user-select: none; 
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`
+
+const SearchBtn = styled.button`
+  cursor: pointer;
+  background-color: #007BE9;
+  position: absolute;
+  top: .2rem;
+  right: .3rem;
+  border: 0;
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+`
+
+export default SearchBar;
+
+
